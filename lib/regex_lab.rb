b9522861f7301b548e96a4964115838b1c80cfec
Elyse Burmester <burmesterelyse@gmail.com>
2018-11-15 01:17:21 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,10 @@
 def starts_with_a_vowel?(word)
-  ary = []
-  ary << /^[aeiou]/.match(word)
-  if ary.length > 0 
-    return true 
-  end
-  other_ary = []
-  other_ary << /^[^aeiou]/.scan(word)
-  if other_ary.length > 0 
-    return false 
-  end 
+
 end
 
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\Aun\zing/)
+  text.scan(/\bun\w+ing\b/)
 end
 
 def words_five_letters_long(text)
